a3012e3123db7ad608dbff30e14fa5c4ce1a8061
José A. Sánchez <jasbinformatica@gmail.com>
2024-10-15 13:36:05 +0200

    Definicio de models i menus (views)

--- models/models.py
+++ models/models.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
+class Player(models.Model):
+    _name = 'galactic_tribals.player'
+    _description = 'Player Model for Galactic Tribals'
 
-# class galactic_tribals(models.Model):
-#     _name = 'galactic_tribals.galactic_tribals'
-#     _description = 'galactic_tribals.galactic_tribals'
-
-#     name = fields.Char()
+    name = fields.Char()
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
@@ -17,3 +16,44 @@
 #         for record in self:
 #             record.value2 = float(record.value) / 100
 
+class Tribu(models.Model):
+    _name = 'galactic_tribals.tribu'
+    _description = 'Tribu Model for Galactic Tribals'
+
+    name = fields.Char()
+
+class Planeta(models.Model):
+    _name = 'galactic_tribals.planeta'
+    _description = 'Planeta Model for Galactic Tribals'
+
+    name = fields.Char()
+
+class Recurs(models.Model):
+    _name = 'galactic_tribals.recurs'
+    _description = 'Recurs Model for Galactic Tribals'
+
+    name = fields.Char()
+
+class Construccio(models.Model):
+    _name = 'galactic_tribals.construccio'
+    _description = 'Construccio Model for Galactic Tribals'
+
+    name = fields.Char()
+
+class Nau(models.Model):
+    _name = 'galactic_tribals.nau'
+    _description = 'Nau Model for Galactic Tribals'
+
+    name = fields.Char()
+
+class Batalla(models.Model):
+    _name = 'galactic_tribals.batalla'
+    _description = 'Batalla Model for Galactic Tribals'
+
+    name = fields.Char()
+
+class Alianza(models.Model):
+    _name = 'galactic_tribals.alianza'
+    _description = 'Alianza Model for Galactic Tribals'
+
+    name = fields.Char()
